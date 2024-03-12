d222a17f5572639ebb440149a6c55449424afdfe
HungDong <narutodjh@gmail.com>
2024-03-12 22:17:40 +0700

    daily streak 15

---
+++ 1171.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+    Solution()
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    ListNode *removeZeroSumSublists(ListNode *head)
+    {
+        ListNode dummy(0, head);
+        int prefix = 0;
+        unordered_map<int, ListNode *> prefixToNode;
+        prefixToNode[0] = &dummy;
+
+        for (; head; head = head->next)
+        {
+            prefix += head->val;
+            prefixToNode[prefix] = head;
+        }
+
+        prefix = 0;
+
+        for (head = &dummy; head; head = head->next)
+        {
+            prefix += head->val;
+            head->next = prefixToNode[prefix]->next;
+        }
+
+        return dummy.next;
+    }
+};
