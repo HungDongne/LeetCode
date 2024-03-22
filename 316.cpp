f55ec32bca3eaad5b240c8c9c6302ef03761148a
HungDong <narutodjh@gmail.com>
2024-03-22 08:48:54 +0700

    Solved 316 (BEATS 100% USERS C++)

---
+++ 316.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    string removeDuplicateLetters(string s)
+    {
+        vector<int> cnt(26, 0);
+        vector<bool> visited(26, false);
+        for (auto c : s)
+        {
+            ++cnt[c - 'a'];
+        }
+        string res = "0";
+        for (auto c : s)
+        {
+            --cnt[c - 'a'];
+            if (visited[c - 'a'])
+            {
+                continue;
+            }
+            while (c < res.back() && cnt[res.back() - 'a'] > 0)
+            {
+                visited[res.back() - 'a'] = false;
+                res.pop_back();
+            }
+            res += c;
+            visited[c - 'a'] = true;
+        }
+        res.erase(res.begin());
+        return res;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    cout << solution.removeDuplicateLetters("cbacdcbc") << endl;
+    return 0;
+}
