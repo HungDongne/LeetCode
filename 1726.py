e06b1246a3f11ab154bddd83e57d7b8802cef2e0
HungDong <115634972+HungDongne@users.noreply.github.com>
2025-07-05 16:36:44 +0700

    Feat: Optimize 1726: Beat 100% users Python

--- 1726.py
+++ 1726.py
@@ -4,12 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        products = defaultdict(int)
+        # BEAT 69.37% USERS PYTHON
+        # products = defaultdict(int)
+        # n = len(nums)
+        # for i in range(n - 1):
+        #     for j in range(i + 1, n):
+        #         products[nums[i] * nums[j]] += 1
+        # res = 0
+        # for product in products.values():
+        #     res += (product * (product - 1)) * 4
+        # return res
+        
+        # BEAT 100% USERS PYTHON
+        products = {}
+        res = 0
         n = len(nums)
         for i in range(n - 1):
             for j in range(i + 1, n):
-                products[nums[i] * nums[j]] += 1
-        res = 0
-        for product in products.values():
-            res += (product * (product - 1)) * 4
+                product = nums[i] * nums[j]
+                if product in products:
+                    res += products[product] * 8
+                    products[product] += 1
+                else:
+                    products[product] = 1
         return res
