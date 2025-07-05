9c7e409157d42f1ba659456a0789cf36ccd91561
HungDong <115634972+HungDongne@users.noreply.github.com>
2025-07-05 17:17:21 +0700

    Solved 416: Beat 75.9% users Python

---
+++ 416.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s = sum(nums)
+        if (s % 2 == 1):
+            return False
+        target = s // 2
+        n = len(nums)
+        dp = [False] * (target + 1)
+        dp[0] = True
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
+        return dp[target]
